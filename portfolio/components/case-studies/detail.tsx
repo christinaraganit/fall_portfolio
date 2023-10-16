5cdd926e06f742f794baf5f45485c540c4527692
Christina Raganit <craganit@my.bcit.ca>
2023-10-16 09:53:10 -0700

    hiii

--- portfolio/components/case-studies/detail.tsx
+++ portfolio/components/case-studies/detail.tsx
@@ -6,7 +6,7 @@ interface Props {
 export default function Detail(props: Props) {
   return (
     <div className="prose pb-4">
-      <span className="block text-slate-900/75 prose-sm">{props.title}</span>
+      <span className="block text-slate-900/80 prose-sm">{props.title}</span>
       <span className="block prose-base">{props.text}</span>
     </div>
   );
--- portfolio/components/nav.tsx
+++ portfolio/components/nav.tsx
@@ -55,7 +55,7 @@ export default function Nav() {
         </ul>
       </nav>
       <nav
-        className="fixed top-4 right-4 pointer-events-auto md:hidden"
+        className="fixed mx-4 my-8 right-4 pointer-events-auto md:hidden"
       >
         <button
           className="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-black-800/5 ring-1 ring-zinc-900/5 backdrop-blur"
@@ -63,10 +63,11 @@ export default function Nav() {
         >
           Menu
         </button>
+        
         <Dialog as="div" onClose={() => setModalOpen(false)} open={modalOpen}>
           {/* The backdrop, rendered as a fixed sibling to the panel container */}
           <div className="fixed inset-0 z-50 bg-zinc-800/40 backdrop-blur-sm opacity-100" aria-hidden="true" />
-          <Dialog.Panel className="fixed inset-x-4 top-8 z-50 origin-top rounded-3xl bg-white p-8 ring-1 ring-zinc-900/5 opacity-100 scale-100">
+          <Dialog.Panel className="fixed inset-x-4 top-4 my-8 z-50 origin-top rounded-3xl bg-white p-8 ring-1 ring-zinc-900/5 opacity-100 scale-100">
             <ul className="flex flex-col">
               <li className="flex justify-end">
                 <a>
