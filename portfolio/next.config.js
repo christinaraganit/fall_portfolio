a6408ff5433a6fe9a197231b56808b7e521ca1ed
RiceDam <dam.eric3@gmail.com>
2023-10-30 02:20:23 -0700

    removed unneeded paths

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/fall_portfolio',
-    assetPrefix: '/fall_portfolio/',
 }
 
 module.exports = nextConfig
